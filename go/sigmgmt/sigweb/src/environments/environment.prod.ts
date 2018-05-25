2376fc845eab61547f84604bc23e554dedf722a3
Lukas Bischofberger <me@worx.li>
2018-05-25 13:07:19 +0200

    Prod api domain same as static (#120)

--- go/sigmgmt/sigweb/src/environments/environment.prod.ts
+++ go/sigmgmt/sigweb/src/environments/environment.prod.ts
@@ -1,9 +1,8 @@
 import { LogLevel } from '../app/api/http-interceptors/log-level'
-import { PROD_URL } from './prod-url'
 
 export const environment = {
   production: true,
   logLevel: LogLevel.Error,
-  domain: PROD_URL,
-  url: PROD_URL === '' ? PROD_URL : 'https://' + PROD_URL
+  domain: '',
+  url: ''
 }
